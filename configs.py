653cd34baa91fe67905ae7a88b8fbeaf60b6eaae
mazhavilmovies <m2185351@gmail.com>
2025-12-07 23:00:01 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 from os import getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "20793620"))
-    API_HASH = getenv("API_HASH", "a712d2b8486f26c4dee5127cc9ae0615")
-    BOT_TOKEN = getenv("BOT_TOKEN", "8145605245:AAHq3JpOz1-5JyObbIsWYlKbBt_bZMuBIEo")
+    API_ID = int(getenv("API_ID", "21419016"))
+    API_HASH = getenv("API_HASH", "79198e1eb4cfd0f771a89d83b9144e7e")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8195734162:AAEDqrvuIgVqnPqEJ4yiDcwtMUW9K8lM3mQ")
     
     # Your Force Subscribe Channel Id
-    CHID = int(getenv("CHID", "-1002581144687"))  # Make the bot admin in this channel
+    CHID = int(getenv("CHID", "-1002674451609"))  # Make the bot admin in this channel
     
     # Bot owners (use space-separated user IDs in env)
-    SUDO = list(map(int, getenv("SUDO", "6853851676").split()))
+    SUDO = list(map(int, getenv("SUDO", "1933114137").split()))
     
     # MongoDB connection URI
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://pokemonchannel098:yaE7BvFwWIXdb3HQ@cluster0.gdr57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://kentkouhali5l_db_user:gFvGsyASnQPu9rDZ@cluster0.m9xgtlr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 cfg = Config()
